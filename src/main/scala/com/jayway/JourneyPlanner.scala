3348cb483f901da1c2622ca12a648747e9a4ca1c
Amir Moulavi <amir.moulavi@gmail.com>
2011-09-29 09:37:21 +0200

    Stops at journey planner is added

--- src/main/scala/com/jayway/JourneyPlanner.scala
+++ src/main/scala/com/jayway/JourneyPlanner.scala
@@ -8,8 +8,14 @@ case class JourneyPlanner(trains:Set[Train]) {
 
   val stations:Set[Station] = trains.flatMap { _.stations }
 
-  def trainsAt(st:Station) = {
-    trains.filter( _.stations.contains(st) )
+  def trainsAt(st:Station):Set[Train] = {
+    trains.filter { _.stations.contains(st) }
+  }
+
+  def stopsAt(st:Station):Set[(Time, Train)] = {
+    for { train <- trains
+          (time, station) <- train.schedule if station == st
+        } yield (time, train)
   }
 
 }
--- src/test/scala/com/jayway/JourneyPlannerSpec.scala
+++ src/test/scala/com/jayway/JourneyPlannerSpec.scala
@@ -12,7 +12,8 @@ class JourneyPlannerSpec extends Specification { def is =
 
   "JourneyPlanner specification" ^
     "Stations should be initialized properly" ! testStations ^
-    "Should check the existence of a station" ! testStationExistence
+    "Should check the existence of a station" ! testStationExistence ^
+    "Should return the (time, train) for a specific station" ! testStopsAt
   end
 
   def testStations = JourneyPlanner(
@@ -26,6 +27,10 @@ class JourneyPlannerSpec extends Specification { def is =
          Train("Nighttrain", "fd3gdgj", Seq( (Time(2,30),Station("Malmo")), (Time(7,45), Station("Lund")) )))
   ).trainsAt(Station("Malmo")).size mustEqual 1
 
+  def testStopsAt = JourneyPlanner(
+    Set( Train("Nighttrain", "fddskj", Seq( (Time(12,30),Station("Stockholm")), (Time(18,45), Station("Lund")) )),
+         Train("Nighttrain", "fd3gdgj", Seq( (Time(2,30),Station("Malmo")), (Time(7,45), Station("Lund")) )))
+  ).stopsAt(Station("Stockholm")).size mustEqual 1
 
 
 }
